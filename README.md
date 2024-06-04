569d1849591818272e8953414d3121e5b409c4ab
wuzijie <jieziwu3202@hotmail.com>
2024-06-04 18:45:06 +0800

    two test

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Command-Line-Tool---Personal-Task-Manager
 Command Line Tool - Personal Task Manager
+A good project is getting started
--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::cout << "A good project is getting started" << std::endl;
     return 0;
 }
