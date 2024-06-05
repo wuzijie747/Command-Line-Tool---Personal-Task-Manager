f4e0f054a153407a681ad2be22b9428b6261ac6e
wuzijie <jieziwu3202@hotmail.com>
2024-06-05 21:19:17 +0800

    main

---
+++ library/Create.h
@@ -0,0 +1,15 @@
+//
+// Created by wuzijie on 2024/6/5.
+//
+
+#ifndef UNTITLED4_CREATE_H
+#define UNTITLED4_CREATE_H
+
+#endif //UNTITLED4_CREATE_H
+
+#include "string"
+using namespace std;
+bool create(const string& task_name,const string& task_description,int year = 0,int month = 0,int day =0,int hour = 0,int minute=0)
+{
+
+}
--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include "library/Create.h"
 using namespace std;
 int main() {
     cout << "A good project is getting started" <<endl;
