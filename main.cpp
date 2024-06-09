c3975ed3afd47a486eb1ebf15302b994b8a2402f
wuzijie <jieziwu3202@hotmail.com>
2024-06-09 13:08:57 +0800

    main

--- main.cpp
+++ main.cpp
@@ -12,7 +12,7 @@ int main()
     system("chcp 65001 && cls");
     system("title Task Manager");
     int Add,Delete,View,mark;
-    print("任务管理器 [版本 1.0.0]\n(c) github 黄文卓,王信，邬子杰。保留所有权力。");
+    print("任务管理器 [版本 1.0.0]\n(c) github 黄文卓,王信，邬子杰。保留所有权力。\n");
     string input;
     map<std::string, std::function<void()>> actionMap {
             // 使用 std::pair 插入元素
